2eb12a586f37a8562f2718b43d3da544e422899a
Arturo Silvelo <asilvelo@trynewroads.com>
2025-10-14 12:57:13 +0200

    refactor: remove request logging middleware and clean up app.js

--- app/src/config/request/interceptors.js
+++
@@ -1,8 +0,0 @@
-const logger = require('../logger');
-
-function requestLogger(req, res, next) {
-  logger.info(`Incoming request: ${req.method} ${req.originalUrl} - IP: ${req.ip}`);
-  next();
-}
-
-module.exports = requestLogger;
